d352637e4cfcacc9f641b4fe39e4f819a4ef6eed
linuxpi <varunb94@gmail.com>
2018-04-14 18:15:32 +0530

    add deploy.py to run application on server

---
+++ viewtubejuggernaut/deploy.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for viewtubejuggernaut project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+path = '/home/varunb94/viewtubejuggernaut'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "viewtubejuggernaut.settings")
+
+application = get_wsgi_application()
