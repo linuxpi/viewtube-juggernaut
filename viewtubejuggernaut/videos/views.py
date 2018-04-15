72814b8e676349bc035abe672e3a21e0cb1af901
linuxpi <varunb94@gmail.com>
2018-04-15 21:20:56 +0530

    update queryset to hide private vidos

--- viewtubejuggernaut/videos/views.py
+++ viewtubejuggernaut/videos/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Q
 from rest_framework import viewsets, permissions
 
 from ..videos.models import VideoUpload
@@ -14,3 +15,7 @@ class VideoUploadViewSet(viewsets.ModelViewSet):
     serializer_class = VideoUploadSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
+    def get_queryset(self):
+        return VideoUpload.objects.filter(
+            Q(is_private=False) | Q(user=self.request.user if self.request.user.is_authenticated() else 0)
+        )
